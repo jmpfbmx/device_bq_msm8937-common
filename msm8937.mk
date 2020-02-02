b3785a48b735716dca0987d0d4e2085682498fdc
jmpfbmx <jmpf.bmx@gmail.com>
2020-02-02 23:20:46 +0100

    msm8937-common: Fix WIFI build

--- msm8937.mk
+++ msm8937.mk
@@ -222,9 +222,3 @@ PRODUCT_PACKAGES += \
 		libwpa_client \
 		libqsap_sdk \
 		libQWiFiSoftApCfg
-
-# WCNSS
-PRODUCT_COPY_FILES += \
-		$(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-		$(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat \
-		$(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
